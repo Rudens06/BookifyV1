a985054ee611bb4b453f9ce1244f3afccd45ab9d
Reinis Rudens <reinis.rudens.dev@gmail.com>
2023-05-03 09:54:22 +0300

    Add sidemenu on mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,8 +18,8 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
@@ -39,3 +39,11 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
--- config/runtime.exs
+++ config/runtime.exs
@@ -112,4 +112,15 @@ if config_env() == :prod do
   #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
   #
   # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
+
+  config :waffle,
+    storage: Waffle.Storage.S3,
+    bucket: System.get_env("AWS_BUCKET_NAME")
+
+  config :ex_aws,
+    json_codec: Jason,
+    access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+    secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+    region: System.get_env("AWS_REGION")
+
 end
