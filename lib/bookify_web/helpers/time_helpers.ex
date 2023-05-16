d3d88fc9168cb5f0a87a9234b26155c4102ba458
Reinis Rudens <reinis.rudens.dev@gmail.com>
2023-05-16 11:04:21 +0300

    Add time helper functions

--- lib/bookify_web/helpers/time_helpers.ex
+++ lib/bookify_web/helpers/time_helpers.ex
@@ -1,5 +1,6 @@
 defmodule BookifyWeb.Helpers.Time do
 
+  def display_time(nil), do: nil
   def display_time(datetime) do
     string =
       datetime
@@ -11,6 +12,7 @@ defmodule BookifyWeb.Helpers.Time do
     hours <> ":" <> minutes
   end
 
+  def display_date(nil), do: nil
   def display_date(datetime) do
     string =
       datetime
@@ -19,5 +21,23 @@ defmodule BookifyWeb.Helpers.Time do
 
     [date | _time] = String.split(string, " ", trim: true)
     date
+  end
+
+  def from_now(nil), do: nil
+  def from_now(timestamp) do
+    current_datetime = DateTime.utc_now()
+    time_diff = DateTime.diff(current_datetime, timestamp)
+
+    if time_diff < 24 * 60 * 60 do
+      hours =
+        time_diff / (60 * 60)
+        |> floor()
+      "#{hours} hours ago"
+    else
+      days =
+        time_diff / (24 * 60 * 60)
+        |> floor()
+      "#{days} days ago"
+    end
   end
 end
