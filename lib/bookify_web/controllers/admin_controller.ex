cbb1685d5a6e1f74d6fc13325db02b6813a7dfc1
Reinis Rudens <reinis.rudens.dev@gmail.com>
2023-05-16 11:05:06 +0300

    Admin user func

--- lib/bookify_web/controllers/admin_controller.ex
+++ lib/bookify_web/controllers/admin_controller.ex
@@ -5,16 +5,39 @@ defmodule BookifyWeb.AdminController do
   alias Bookify.Book
   alias Bookify.Books
   alias Bookify.Authors
-  alias Bookify.Review
+  alias Bookify.Reviews
+  alias Bookify.Accounts
 
-  plug BookifyWeb.Plugs.RequireAdmin when action in [:index, :new, :create, :edit, :update, :delete]
+  plug BookifyWeb.Plugs.RequireAdmin
 
-  def init(conn, _params) do
+  def init(conn) do
     conn
   end
 
-  def index(conn, _params) do
+  def reviews(conn, _params) do
+    reviews = Reviews.list_reviews()
+
     conn
-    |> render(:index, layout: false)
+    |> assign(:reviews, reviews)
+    |> put_layout({BookifyWeb.Layouts, :admin})
+    |> render(:reviews)
+  end
+
+  def users(conn, _params) do
+    users = Accounts.list_users()
+
+    conn
+    |> assign(:users, users)
+    |> put_layout({BookifyWeb.Layouts, :admin})
+    |> render(:users)
+  end
+
+  def show(conn, %{"id" => user_id}) do
+    user = Accounts.get_user_by_id_w_preloads(user_id)
+
+    conn
+    |> assign(:user, user)
+    |> put_layout({BookifyWeb.Layouts, :admin})
+    |> render(:show)
   end
 end
--- lib/bookify_web/controllers/admin_html.ex
+++ lib/bookify_web/controllers/admin_html.ex
@@ -2,6 +2,7 @@ defmodule BookifyWeb.AdminHTML do
   use BookifyWeb, :html
 
   import Phoenix.HTML.Link
+  import BookifyWeb.Helpers.Time
 
   embed_templates "admin_html/*"
 end
