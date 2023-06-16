6591935ec8ed44a836c2ae0ffdd994302632d713
Reinis Rudens <reinis.rudens.dev@gmail.com>
2023-06-17 00:50:07 +0300

    Add search function

--- lib/bookify/authors.ex
+++ lib/bookify/authors.ex
@@ -1,9 +1,14 @@
 defmodule Bookify.Authors do
+  import Ecto.Query
   alias Bookify.Repo
   alias Bookify.Author
 
-  def list_all() do
-    Repo.all(Author)
+  def list_all(params) do
+    search_term = get_in(params, ["query"])
+
+    Author
+    |> search(search_term)
+    |> Repo.all()
   end
 
   def get_by_id!(author_id) do
@@ -21,5 +26,12 @@ defmodule Bookify.Authors do
   def delete_by_id!(author_id) do
     Repo.get(Author, author_id)
     |> Repo.delete!
+  end
+
+  def search(query, search_term) do
+    wildcard_search = "%#{search_term}%"
+
+    from author in query,
+    where: ilike(author.name, ^wildcard_search)
   end
 end
