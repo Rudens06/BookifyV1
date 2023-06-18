f08774699bf786e2409e441d44189514b130c826
Martins Spilners <martins@ctrld.ch>
2023-06-18 21:51:10 +0300

    Localize titles for flash messages

--- lib/bookify_web/components/core_components.ex
+++ lib/bookify_web/components/core_components.ex
@@ -185,8 +185,8 @@ defmodule BookifyWeb.CoreComponents do
 
   def flash_group(assigns) do
     ~H"""
-    <.flash kind={:info} title="Success!" flash={@flash} />
-    <.flash kind={:error} title="Error!" flash={@flash} />
+    <.flash kind={:info} title={gettext("Success!")} flash={@flash} />
+    <.flash kind={:error} title={gettext("Error!")} flash={@flash} />
     <.flash
       id="disconnected"
       kind={:error}
